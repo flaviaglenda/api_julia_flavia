1899c5b293c0a4c59a07841a8a06d44b0ebdb934
JuliaConconi <conconijulia@gmail.com>
2025-05-06 12:31:45 -0300

    .

--- src/screens/inicio.js
+++ src/screens/inicio.js
@@ -1,4 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { View, TextInput, Button, Stylesheet } from 'react-native-web';
 import * as Location from 'expo-location';
-import MapView, {Maker} from 'react-native-maps'
+import MapView, {Maker} from 'react-native-maps';
+
+export default funcion HomeScreen({navigation}) {
+  const [location, setLocation] = useState(null);
+  const [destination, setDestination] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await Location.requestBackgroundPermissionsAsync();
+      if (status != 'granted') return;
+      const loc = await Location.getCurrentPositionAsync({});
+      setLocation(loc.coords);
+    })();
+  }, []);
+
+  const hanldeNavigate = () => {
+    if (!destination || !location) return;
+    navigation.navigate('Route', {destination, origin: location});
+  };
+}
